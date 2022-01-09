bd4d7ebdad1a37733c9bfcae459a67b322cdc641
kdemirta <keremdemirtas@gmail.com>
2022-01-09 15:22:18 +0300

    initial ga run

--- max_coverage_ga_with_pip_package/solver.py
+++ max_coverage_ga_with_pip_package/solver.py
@@ -8,9 +8,24 @@ import geneticalgorithm.geneticalgorithm as ga
 ROOT_DIR = os.path.dirname(os.path.dirname(__file__))
 FILES_DIR = os.path.join(ROOT_DIR, "files")
 
+# Parse settings
+file_path = os.path.join(FILES_DIR, "settings.json")
+with open(file_path) as fp:
+    inp = json.load(fp)
+
+input_settings = inp["settings"]
+max_coverage_distance = input_settings["max_coverage_distance"]
+K = input_settings["max_facilities"]
+N = input_settings["n_locations_demand"]
+M = input_settings["n_locations_possible_facility"]
+maximum_generations = input_settings["ga_settings"]["maximum_generations"]
+mutation_probability = input_settings["ga_settings"]["mutation_probability"]
+population_size = input_settings["ga_settings"]["population_size"]
+
 # Parse distances
 file_path = os.path.join(FILES_DIR, "Distance Matrix.xlsx")
 distances = pd.read_excel(file_path, "distances", header=None).values
+coverage_matrix = np.where(distances <= max_coverage_distance, 1, 0)
 
 # Parse demand
 file_path = os.path.join(FILES_DIR, "Project Data.xlsx")
@@ -20,26 +35,51 @@ demand = pd.read_excel(file_path, "demand", header=None).values
 file_path = os.path.join(FILES_DIR, "Project Data.xlsx")
 capacity = pd.read_excel(file_path, "capacity", header=None).values
 
-# Parse settings
-file_path = os.path.join(FILES_DIR, "settings.json")
-with open(file_path) as fp:
-    inp = json.load(fp)
-
-input_settings = inp["settings"]
+# Parse info
+file_path = os.path.join(FILES_DIR, "Project Data.xlsx")
+info = pd.read_excel(file_path, "info", header=None).values
 
 
 def f(X):
-    yi = X[28:55]
-    obj_value = np.sum(yi * demand)
+    x = X[0:28]
+    y = X[28:56]
+    z = X[56:840]
+    obj_value = np.sum(y * demand)
 
-    # Coverage constraint penalty
+    coverage_set = coverage_matrix.flatten()
 
-    return -obj_value
+    # Demand coverage constraint penalty
+    demand_coverage_penalty = 0.0
+    pen_coverage = []
+    for idx, yi in enumerate(y):
+        if yi > np.sum(z):
+            # TODO sum over its own set the whole set of facilities
+            pen = 500 + 1000 * (yi - np.sum(z * coverage_set))
+            pen_coverage.append(pen)
+    demand_coverage_penalty = sum(pen_coverage)
+
+    # Max number of facilities constraint penalty
+    max_facility_penalty = 0.0
+    if np.sum(x) > K:
+        max_facility_penalty = 500 + 1000 * (np.sum(K) - K)
+
+    # Capacity constraint penalty
+
+    return -obj_value + demand_coverage_penalty + 500 * max_facility_penalty
+
+def report(output_dict):
+    X_best = output_dict["variable"]
+    f_best = output_dict["function"]
+    x_optimal = np.array(X_best[0:28])
+    y_optimal = np.array(X_best[28:56])
+    z_optimal = np.array(X_best[56:840]).reshape([28, 28])
+
+    print("x_optimal:", x_optimal, "\n", "y_optimal:", y_optimal, "\n", "z_optimal:", z_optimal, "\n")
 
 algorithm_param = {
-    'max_num_iteration': 1000,
-    'population_size':100,
-    'mutation_probability':0.1,
+    'max_num_iteration': maximum_generations,
+    'population_size': population_size,
+    'mutation_probability': mutation_probability,
     'elit_ratio': 0.01,
     'crossover_probability': 0.5,
     'parents_portion': 0.3,
@@ -49,4 +89,7 @@ algorithm_param = {
 
 model=ga(function=f, dimension=840, variable_type='bool', function_timeout=1000, algorithm_parameters=algorithm_param)
 output_dict = model.run()
+report(output_dict)
+
+
 print("Done")
